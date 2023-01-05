2e9b3d6d1eee644c49b101384b454a806c9f083a
othonss <othon.santos.silva@gmail.com>
2023-01-05 20:37:48 -0300

    APLICANDO DESCONTOS NOS PREÇOS DOS LIVROS UTILIZANDO MÉTODO MAP DO JS

--- Formação - JavaScript para Front-end/JavaScript - Métodos de array/alura_books-projeto_inicial/app/main.js
+++ Formação - JavaScript para Front-end/JavaScript - Métodos de array/alura_books-projeto_inicial/app/main.js
@@ -8,7 +8,8 @@ async function getBuscarLivrosDaAPI(){
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
     // achei interessante a variação do console :D console.table(livros)
-    exibirOsLivrosNaTela(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
 
 function exibirOsLivrosNaTela(listaDeLivros){
@@ -21,11 +22,19 @@ function exibirOsLivrosNaTela(listaDeLivros){
             ${livro.titulo}
         </h2>
         <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
         <div class="tags">
             <span class="tag">${livro.categoria}</span>
         </div>
         </div>
       `  
     })
+}
+
+function aplicarDesconto(livros){
+    const desconto = 0.3
+    livrosComDesconto = livros.map(livro =>{
+        return {...livro, preco: livro.preco - (livro.preco * desconto)} // NOVIDADE -> operador ...
+    })
+    return livrosComDesconto
 }
