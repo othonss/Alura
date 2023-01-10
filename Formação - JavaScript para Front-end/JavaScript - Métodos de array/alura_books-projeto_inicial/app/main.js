3e3906389e4c5861230b9a5a509d5d17ed588bcb
othonss <othon.santos.silva@gmail.com>
2023-01-10 19:27:15 -0300

    ORDENANDO LIVROS COM O MÉTODO SORT

--- Formação - JavaScript para Front-end/JavaScript - Métodos de array/alura_books-projeto_inicial/app/main.js
+++ Formação - JavaScript para Front-end/JavaScript - Métodos de array/alura_books-projeto_inicial/app/main.js
@@ -2,6 +2,7 @@ let livros = []
 const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 const botoes = document.querySelectorAll('.btn')
 
+
 getBuscarLivrosDaAPI()
 const elementoParaInserirLivros = document.getElementById('livros')
 
@@ -14,25 +15,38 @@ async function getBuscarLivrosDaAPI(){
     exibirOsLivrosNaTela(livrosComDesconto)
 }
 
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
 function exibirOsLivrosNaTela(listaDeLivros){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
-      elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
-            alt="${livro.alt}" />
-        <h2 class="livro__titulo">
-            ${livro.titulo}
-        </h2>
-        <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
-        <div class="tags">
-            <span class="tag">${livro.categoria}</span>
-        </div>
-        </div>
-      `  
+        //let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro_imagens' : 'livros_imagens indisponivel'
+        elementoParaInserirLivros.innerHTML += `
+            <div class="livro">
+            <img class="${disponibilidade}" src="${livro.imagem}"
+                alt="${livro.alt}" />
+            <h2 class="livro__titulo">
+                ${livro.titulo}
+            </h2>
+            <p class="livro__descricao">${livro.autor}</p>
+            <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+            <div class="tags">
+                <span class="tag">${livro.categoria}</span>
+            </div>
+            </div>
+        `  
     })
 }
+
+/*function verificarDisponibilidadeDoLivro(livro){
+    if(livro.quantidade >0){
+        return 'livro_imagens'
+    }else{
+        return 'livros_imagens indisponivel'
+    }
+}*/
 
 function aplicarDesconto(livros){
     const desconto = 0.3
@@ -47,6 +61,33 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros(){
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel'){
+        exibirValorTotalDosLivrosDisponiveisNaTela()
+    }
+}
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela(){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+        </div>
+    `
+}
+
+let btnOrdenarPorPreco = document.getElementById('btnOrdenarPorPreco')
+btnOrdenarPorPreco.addEventListener('click', ordernarLivrosPorPreco)
+
+function ordernarLivrosPorPreco(){
+    let livrosOrdenados = livros.sort((a, b) => a.preco - b.preco)
+    exibirOsLivrosNaTela(livrosOrdenados)
 }
