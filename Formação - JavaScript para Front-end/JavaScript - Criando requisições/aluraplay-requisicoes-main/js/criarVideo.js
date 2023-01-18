982d6965f33ffa1670769eb813c87934f78f7eb5
othonss <othon.santos.silva@gmail.com>
2023-01-18 20:03:22 -0300

    ENVIANDO VÍDEOS ATRAVÉS DO FORMULÁRIO - Foi criado a função resposavél para enviar o vídeos com o método POST - Foi criado o arquivo js responsável por gerenciar o envio de vídeos

---
+++ Formação - JavaScript para Front-end/JavaScript - Criando requisições/aluraplay-requisicoes-main/js/criarVideo.js
@@ -0,0 +1,17 @@
+import { conectaApi } from "./conectaAPI.js"
+
+const formulario = document.querySelector("[data-formulario]")
+
+async function criarVideo(evento){
+    evento.preventDefault() /* Previne o comportamento padrão de dar refresh na página */
+    const imagem = document.querySelector("[data-imagem]").value
+    const url = document.querySelector("[data-url]").value
+    const titulo = document.querySelector("[data-titulo]").value
+    const descricao = Math.floor(Math.random() * 10).toString()
+
+    await conectaApi.criaVideo(titulo, descricao, url, imagem) /* Importante a ordem ser igual ao que foi especificado na função */
+
+    window.location.href = "../pages/envio-concluido.html"
+}
+
+formulario.addEventListener("submit", evento => criarVideo(evento))
