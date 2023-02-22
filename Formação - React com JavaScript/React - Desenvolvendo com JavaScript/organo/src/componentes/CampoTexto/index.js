956aef1a2694cfb2c59ced75d8013912ff0b4324
othonss <othon.santos.silva@gmail.com>
2023-02-22 20:57:24 -0300

    CAPTURANDO INFORMAÇÕES DO FORMULÁRIO COM USESTATE

--- Formação - React com JavaScript/React - Desenvolvendo com JavaScript/organo/src/componentes/CampoTexto/index.js
+++ Formação - React com JavaScript/React - Desenvolvendo com JavaScript/organo/src/componentes/CampoTexto/index.js
@@ -1,10 +1,18 @@
 import './CampoTexto.css'
 const CampoTexto = (props) => {
     const placeholderModificada = `${props.placeholder}...`
+
+    // let valor = 'Othon'
+    
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
