73d93c99b04a56c299f6058c7cffd2983a486b5e
othonss <othon.santos.silva@gmail.com>
2023-03-15 19:23:29 -0300

    FINALIZANDO COMPONENTE DO TIME - Foi criado mais um componente chamado Colaborador; - Foi estilizado o componente Colaborador; - Foi desenvolvido a lógica para criar os colaboradores de maneira dinâmica; - Foi criado a lógica para limpar o formulário e esconder times que não há colabores cadastrados.

--- Formação - React com JavaScript/React - Desenvolvendo com JavaScript/organo/src/componentes/Time/index.js
+++ Formação - React com JavaScript/React - Desenvolvendo com JavaScript/organo/src/componentes/Time/index.js
@@ -1,12 +1,17 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
     const backgroundSection = { backgroundColor: props.corSecundaria }
     const borderTitle = { borderColor: props.corPrimaria }
     return (
-        <section className="time" style={backgroundSection}>
+       (props.colaboradores.length > 0) ? <section className="time" style={backgroundSection}>
             <h3 style={borderTitle}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)} 
+            </div>
         </section>
+        : ''
     )
 }
 
